dc1949272d143b6c66f4f796bc5142336424042e
Muhammed Shafaf <shafafmuhammad6@gamil.com>
2021-09-22 18:54:36 +0530

    Completed

--- stylejs.js
+++ stylejs.js
@@ -1,24 +1,25 @@
-function formValidation() {  //function for form validation
+/*function for form validation*/
+function formValidation() {
     var Email = document.getElementById('email_address').value;
     var Password= document.getElementById('password_field').value;
     if (Email == "") {
-    alert("Please enter the email address.")
-    window.location="Login.html"
+    alert("Please enter the email address.");
+    window.location="Login.html";
     } else if (Password=="") {
-    alert("Enter your password.")
-      window.location="Login.html"
+    alert("Enter your password.");
+      window.location="Login.html";
     } else {
-      window.location="home.html"
+      window.location="home.html";
     }
 }
-
-function startCounter(){  // function for counting, when the user login
-    let seconds = 10
-    let total = 0
+/* function for counting, when the user login */
+function startCounter(){
+    let seconds = 10;
+    let total = 0;
     for (let i = 0; i <= 10; i++) {
       setTimeout(function timer() {
-        let total = seconds -i 
-        document.getElementById("timer").innerHTML =  "00 : "+total
+        let total = seconds -i;
+        document.getElementById("timer").innerHTML =  "00 : "+total;
 
       }, i * 1000);
     }
