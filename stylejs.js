836467320047b05e637146f5196ddc2082a9f8a5
Muhammed Shafaf <shafafmuhammad6@gamil.com>
2021-09-22 14:53:55 +0530

    Temporory commit (logo image change)

--- stylejs.js
+++ stylejs.js
@@ -1,35 +1,25 @@
-console.log("success")
-function formValidation(){
-  var Email = document.getElementById('Email').value;
-  var Password= document.getElementById('Password').value;
-
-  if (Email == "") {
-  //  block of code to be executed if condition1 is true
-  alert("Please enter the email address.")
-  window.location="Login.html"
-} else if (Password=="") {
-  alert("Enter your password.")
-  window.location="Login.html"
-  //  block of code to be executed if the condition1 is false and condition2 is true
-} else {
-  //  block of code to be executed if the condition1 is false and condition2 is false
-  window.location="home.html"
-
-}
+function formValidation() {  //function for form validation
+    var Email = document.getElementById('email_address').value;
+    var Password= document.getElementById('password_field').value;
+    if (Email == "") {
+    alert("Please enter the email address.")
+    window.location="Login.html"
+    } else if (Password=="") {
+    alert("Enter your password.")
+      window.location="Login.html"
+    } else {
+      window.location="home.html"
+    }
 }
 
+function startCounter(){  // function for counting, when the user login
+    let seconds = 10
+    let total = 0
+    for (let i = 0; i <= 10; i++) {
+      setTimeout(function timer() {
+        let total = seconds -i 
+        document.getElementById("timer").innerHTML =  "00 : "+total
 
-function startCounter(){
-  console.log("hello")
-  let num = 10
-  let total = 0
-  for (let i = 0; i <= 10; i++) {
-    setTimeout(function timer() {
-      let total = num -i
-      document.getElementById("timer").innerHTML = num-i + "seconds"
-      if (total == 0 ){
-        document.getElementById("show").innerHTML = "let's start"
-      }
-    }, i * 1000);
-  }
+      }, i * 1000);
+    }
   }
